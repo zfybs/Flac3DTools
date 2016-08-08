a8fecf96e8c567d6126e63e59eee6c8cc511eaae
zfybs <619086871@qq.com>
2016-08-08 15:22:11 +0800

    添加某些异常的处理

--- HM2FL3D/My Project/Settings.Designer.vb
+++ HM2FL3D/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Hm2Flac3D.My.MySettings
+        Friend ReadOnly Property Settings() As Global.My.MySettings
             Get
-                Return Global.Hm2Flac3D.My.MySettings.Default
+                Return Global.My.MySettings.Default
             End Get
         End Property
     End Module
